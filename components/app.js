4bcbede2e2c2fa35ef2202a1b294fb963420b529
Andy <andrewryzen@gmail.com>
2020-06-11 12:27:52 -0400

    added components and imgs folder

---
+++ components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import Footer from './components/Footer';
+
+class App extends Components {
+    render(){
+        return (
+            <div>
+            <Navbar />
+            <Footer />
+            </div>
+        );
+    }
+}
