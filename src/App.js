d26d7c61b2e0596a83171fcc49a6913150d50556
Andy <andrewryzen@gmail.com>
2020-06-12 13:10:07 -0400

    Created footer unfinished

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { render } from '@testing-library/react';
 import Header from './Components/Header';  
+import Footer from './Components/Footer';  
 
 
 
 class App extends Component {
 render() {
 return (
-    <div className="App">
 
-      <Header/>
-      
+    <div>
+      <Header />
+      <Footer />
     </div>
     );
   }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,1 +1,15 @@
+import React from 'react';
+export default Footer;
 
+
+function Footer() {
+  return (
+    
+          <div>
+            <h1>
+               Copyright © 2020 Andrew Gonzalez All Rights Reserved
+            </h1>
+          </div>
+      
+    );
+  }
