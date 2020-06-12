fc3ca1591e9e5352dc3b69db79bd7a7e797ff4a2
Andy <andrewryzen@gmail.com>
2020-06-12 13:44:24 -0400

    Footer is now sticky

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,18 @@
+import './style.css';
 import React from 'react';
 export default Footer;
 
 
+
 function Footer() {
   return (
+    <div className="Footer">
     
           <div>
             <h1>
                Copyright © 2020 Andrew Gonzalez All Rights Reserved
             </h1>
           </div>
-      
+          </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { render } from '@testing-library/react';
-  
 export default class Header extends Component {
 render() {
 return (
