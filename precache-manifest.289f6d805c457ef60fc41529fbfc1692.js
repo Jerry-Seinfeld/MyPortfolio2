b6ef20cb5a964aeee4210f29b973e78b6ceb2663
Andy <andrewryzen@gmail.com>
2020-06-13 14:33:41 -0400

    Updates

---
+++ precache-manifest.289f6d805c457ef60fc41529fbfc1692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a85f7ec787e7e6c7cb2b0733346df2e",
+    "url": "/MyPortfolio2/index.html"
+  },
+  {
+    "revision": "cb5ae211c4c8dab371f8",
+    "url": "/MyPortfolio2/static/css/main.be79ba6b.chunk.css"
+  },
+  {
+    "revision": "28843a36f531d5555e69",
+    "url": "/MyPortfolio2/static/js/2.772eb646.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/MyPortfolio2/static/js/2.772eb646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5ae211c4c8dab371f8",
+    "url": "/MyPortfolio2/static/js/main.51a2403a.chunk.js"
+  },
+  {
+    "revision": "5e3bb8dbf547134bd7b0",
+    "url": "/MyPortfolio2/static/js/runtime-main.52b4acc3.js"
+  }
+]);
